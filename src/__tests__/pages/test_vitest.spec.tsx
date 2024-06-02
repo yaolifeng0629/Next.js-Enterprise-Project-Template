a777dd818c424664a272bb262023270567a11e3e
Immerse <y15829485647@163.com>
2024-06-02 22:22:34 +0800

    perf: adjust log

--- src/__tests__/pages/test_vitest.spec.tsx
+++ src/__tests__/pages/test_vitest.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
 
-import Page from '@/pages/test_vitest';
+import Page from '@/pages/test_vitest/page';
 
 test('Page', () => {
     render(<Page />);
